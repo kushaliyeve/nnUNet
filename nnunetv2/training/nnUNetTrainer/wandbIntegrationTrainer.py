997e5052e6068f3b8d84e625f6e2f186961d567c
Yernur Kushaliyev <yernur.kushaliyev@syndiag.ai>
2025-06-03 16:17:47 +0200

    remove 1 epoch

--- nnunetv2/training/nnUNetTrainer/wandbIntegrationTrainer.py
+++ nnunetv2/training/nnUNetTrainer/wandbIntegrationTrainer.py
@@ -25,9 +25,7 @@ class nnUNetTrainer_Wandb_Logger(nnUNetTrainer):
                  device: torch.device = torch.device('cuda')):
         """used for debugging plans etc"""
         super().__init__(plans, configuration, fold, dataset_json, device)
-        
-        self.num_epochs = 1
-        
+                
         wandb.init(
                 project="pinkcc",
                 name=f"Dataset_001_PINKCC_{self.fold}",
