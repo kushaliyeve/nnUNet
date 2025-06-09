0740394ac161f144b5b7f258460a15c5163e2adf
Yernur Kushaliyev <yernur.kushaliyev@syndiag.ai>
2025-06-09 16:58:30 +0200

    remove 1 epoch for testing

--- nnunetv2/training/nnUNetTrainer/wandbIntegrationTrainer.py
+++ nnunetv2/training/nnUNetTrainer/wandbIntegrationTrainer.py
@@ -26,7 +26,6 @@ class nnUNetTrainer_Wandb_Logger(nnUNetTrainer):
         """used for debugging plans etc"""
         super().__init__(plans, configuration, fold, dataset_json, device)
         
-        self.num_epochs = 1
                 
         wandb.init(
                 project="pinkcc",
